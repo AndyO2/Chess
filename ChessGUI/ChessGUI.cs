a611e965bf8ac937872b05916c5b0532e15484ca
AndyO2 <andy.okamoto@gmail.com>
2022-05-10 17:09:51 -0600

    Able to get pieces moving

--- ChessGUI/ChessGUI.cs
+++ ChessGUI/ChessGUI.cs
@@ -42,11 +42,6 @@ namespace ChessGUI
             this.Paint += Draw_Board;
             DoubleBuffered = true;
 
-            var timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1000 / 30;  // 1000 milliseconds in a second divided by 30 frames per second
-            timer.Tick += (a, b) => this.Invalidate();
-            timer.Start();
-
             InitializeComponent();
         }
 
@@ -60,8 +55,39 @@ namespace ChessGUI
             {
                 for (int row = 0; row < boardSize; row++)
                 {
+                    // create new Panel control which will be one chess board tile
+                    var newPanel = new Panel
+                    {
+                        Size = new Size(tileSize, tileSize),
+                        //The +100 shifts the overall board down to (100,100) top left corner
+                        Location = new Point(tileSize * column + 100, tileSize * row + 100),
+
+                        BackgroundImageLayout = ImageLayout.Center,
+                    };
+
+                    newPanel.Click += Square_Click;
+
+                    //Event handler when a panel is hovered over. Only do something if the square is occupied
+                    newPanel.MouseHover += Square_Hover;
+
+                    // add to our 2d array of panels for future use
+                    chessBoardPanels[column, row] = newPanel;
+
+                    // add to Form's Controls so that they show up
+                    Controls.Add(newPanel);
+
+                    // color the backgrounds
+                    if (column % 2 == 0)
+                    {
+                        newPanel.BackColor = row % 2 != 0 ? clr1 : clr2;
+                    }
+                    else
+                    {
+                        newPanel.BackColor = row % 2 != 0 ? clr2 : clr1;
+                    }
+
                     //White Pawn
-                    if(row == 6)
+                    if (row == 6)
                     {
                         chessBoard[column, row] = new Square(column, row, new Pawn('W', column, row));
                     }
@@ -141,21 +167,6 @@ namespace ChessGUI
             {
                 for (var row = 0; row < gridSize; row++)
                 {
-                    // create new Panel control which will be one chess board tile
-                    var newPanel = new Panel
-                    {
-                        Size = new Size(tileSize, tileSize),
-                        //The +100 shifts the overall board down to (100,100) top left corner
-                        Location = new Point(tileSize * column + 100, tileSize * row + 100),
-
-                        BackgroundImageLayout = ImageLayout.Center,
-                    };
-
-                    newPanel.Click += Square_Click;
-
-                    //Event handler when a panel is hovered over. Only do something if the square is occupied
-                    newPanel.MouseHover += Square_Hover;
-
                     // Access the occupant of this square if there is one (can be null)
                     ChessPiece? occupant = chessBoard[column, row].getOccupant();
 
@@ -163,83 +174,71 @@ namespace ChessGUI
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhitePawn.png");
+                            chessBoardPanels[column,row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhitePawn.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackPawn.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackPawn.png");
                         }
                     }
                     else if (occupant is King)
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteKing.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteKing.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackKing.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackKing.png");
                         }
                     }
                     else if (occupant is Queen)
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteQueen.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteQueen.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackQueen.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackQueen.png");
                         }
                     }
                     else if (occupant is Rook)
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteRook.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteRook.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackRook.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackRook.png");
                         }
                     }
                     else if (occupant is Bishop)
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteBishop.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteBishop.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackBishop.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackBishop.png");
                         }
                     }
                     else if (occupant is Knight)
                     {
                         if (occupant.isWhite())
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteKnight.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\WhiteKnight.png");
                         }
                         else
                         {
-                            newPanel.BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackKnight.png");
+                            chessBoardPanels[column, row].BackgroundImage = Image.FromFile("..\\..\\..\\..\\ChessPieceImages\\BlackKnight.png");
                         }
-                    }
-
-                    // add to our 2d array of panels for future use
-                    chessBoardPanels[column, row] = newPanel;
-
-                    // add to Form's Controls so that they show up
-                    Controls.Add(newPanel);
-
-                    // color the backgrounds
-                    if (column % 2 == 0)
-                    {
-                        newPanel.BackColor = row % 2 != 0 ? clr1 : clr2;
                     }
                     else
                     {
-                        newPanel.BackColor = row % 2 != 0 ? clr2 : clr1;
+                        chessBoardPanels[column, row].BackgroundImage = null;
                     }
                 }
             }
@@ -247,35 +246,24 @@ namespace ChessGUI
 
         private void Square_Click(object? sender, EventArgs e)
         {
-            chessBoard[2, 4].occupant = new King('W', 2, 4);
-
-            Debug.WriteLine($"{chessBoard[2, 4].occupant}");
-            Debug.WriteLine($"{chessBoard[3, 4].occupant}");
-
-
-
             Panel p = (Panel)sender;
-            Debug.WriteLine($"{p.Location.X / 60 - 1} - {p.Location.Y / 60 - 1}");
 
-            //if (currSquareClicked is null)
-            //{
-            //    //currSquareClicked = chessBoard[p.Location.X / 60 - 1, p.Location.Y / 60 - 1];
-            //    currSquareClicked = chessBoard[p.Location.Y / 60 - 1, p.Location.X / 60 - 1];
+            if (currSquareClicked is null)
+            {
+                currSquareClicked = chessBoard[p.Location.X / 60 - 1, p.Location.Y / 60 - 1];
+                currSquareClicked.Row = p.Location.X / 60 - 1;
+                currSquareClicked.Col = p.Location.Y / 60 - 1;
+            }
+            else
+            {
+                chessBoard[p.Location.X / 60 - 1, p.Location.Y / 60 - 1].occupant = currSquareClicked.occupant;
 
-            //    Debug.WriteLine($"{p.Location.X / 60 - 1} - {p.Location.Y / 60 - 1}");
-            //}
-            //else
-            //{
-            //    //chessBoard[p.Location.X / 60 - 1, p.Location.Y / 60 - 1].occupant = currSquareClicked.occupant;
-            //    chessBoard[2, 4].occupant = new Pawn('W', 2 ,4);
+                chessBoard[currSquareClicked.Row,currSquareClicked.Col].occupant = null;
 
-            //    Debug.WriteLine($"{p.Location.X / 60 - 1} - {p.Location.Y / 60 - 1}");
+                currSquareClicked = null;
+            }
 
-            //    //currSquareClicked.occupant = null;
-            //    currSquareClicked = null;
-
-            //    //Debug.WriteLine($"{currSquareClicked.Row} - {currSquareClicked.Col}");
-            //}
+            this.Invalidate();
         }
 
         private void Square_Hover(object? sender, EventArgs e)
