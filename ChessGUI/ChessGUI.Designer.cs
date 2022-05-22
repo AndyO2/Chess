0c41b6a1e48d41caa19d8c4310c7538fd929ce63
AndyO2 <andy.okamoto@gmail.com>
2022-05-22 13:12:43 -0600

    added rank labels and changed background to black

--- ChessGUI/ChessGUI.Designer.cs
+++ ChessGUI/ChessGUI.Designer.cs
@@ -37,87 +37,103 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.GameMessagesTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AFileLabel
             // 
             this.AFileLabel.AutoSize = true;
             this.AFileLabel.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.AFileLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.AFileLabel.Location = new System.Drawing.Point(96, 588);
             this.AFileLabel.Name = "AFileLabel";
-            this.AFileLabel.Size = new System.Drawing.Size(63, 56);
+            this.AFileLabel.Size = new System.Drawing.Size(56, 56);
             this.AFileLabel.TabIndex = 0;
-            this.AFileLabel.Text = "A";
+            this.AFileLabel.Text = "a";
             // 
             // BFileLabel
             // 
             this.BFileLabel.AutoSize = true;
             this.BFileLabel.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BFileLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BFileLabel.Location = new System.Drawing.Point(165, 588);
             this.BFileLabel.Name = "BFileLabel";
-            this.BFileLabel.Size = new System.Drawing.Size(52, 56);
+            this.BFileLabel.Size = new System.Drawing.Size(56, 56);
             this.BFileLabel.TabIndex = 0;
-            this.BFileLabel.Text = "B";
+            this.BFileLabel.Text = "b";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(214, 588);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(221, 588);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 56);
+            this.label1.Size = new System.Drawing.Size(54, 56);
             this.label1.TabIndex = 0;
-            this.label1.Text = "C";
+            this.label1.Text = "c";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(281, 588);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 56);
+            this.label2.Size = new System.Drawing.Size(56, 56);
             this.label2.TabIndex = 0;
-            this.label2.Text = "D";
+            this.label2.Text = "d";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(344, 588);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 56);
+            this.label3.Size = new System.Drawing.Size(55, 56);
             this.label3.TabIndex = 0;
-            this.label3.Text = "E";
+            this.label3.Text = "e";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(401, 588);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(408, 588);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 56);
+            this.label4.Size = new System.Drawing.Size(42, 56);
             this.label4.TabIndex = 0;
-            this.label4.Text = "F";
+            this.label4.Text = "f";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(456, 588);
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(466, 588);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 56);
+            this.label5.Size = new System.Drawing.Size(55, 56);
             this.label5.TabIndex = 0;
-            this.label5.Text = "G";
+            this.label5.Text = "g";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label6.Location = new System.Drawing.Point(527, 588);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 56);
+            this.label6.Size = new System.Drawing.Size(52, 56);
             this.label6.TabIndex = 0;
-            this.label6.Text = "H";
+            this.label6.Text = "h";
             // 
             // GameMessagesTextBox
             // 
@@ -126,11 +142,108 @@
             this.GameMessagesTextBox.Size = new System.Drawing.Size(214, 23);
             this.GameMessagesTextBox.TabIndex = 1;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(49, 405);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 56);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(49, 472);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 56);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(49, 528);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 56);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label10.Location = new System.Drawing.Point(49, 349);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 56);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "4";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label11.Location = new System.Drawing.Point(49, 293);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 56);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "5";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label12.Location = new System.Drawing.Point(49, 227);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 56);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "6";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label13.Location = new System.Drawing.Point(51, 171);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(52, 56);
+            this.label13.TabIndex = 8;
+            this.label13.Text = "7";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("YoonA YoonChe Ultra Light", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label14.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label14.Location = new System.Drawing.Point(51, 104);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(52, 56);
+            this.label14.TabIndex = 9;
+            this.label14.Text = "8";
+            // 
             // Chess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
             this.Controls.Add(this.GameMessagesTextBox);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -159,5 +272,13 @@
         private Label label5;
         private Label label6;
         private TextBox GameMessagesTextBox;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
     }
 }
